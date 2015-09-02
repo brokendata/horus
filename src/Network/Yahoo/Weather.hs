4522a52cc36c36a65473711f99b371dbbc8e9e4f
Ryan Wheeler <ryanmwheeler@gmail.com>
2015-09-01 19:44:28 -0500

    validate aeson parser

--- src/Network/Yahoo/Weather.hs
+++ src/Network/Yahoo/Weather.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-import qualified Data.Text as T
-import Data.ByteString.Lazy as LBS
-
+import           Control.Applicative  ((<$>), (<*>))
+import           Control.Lens
+import           Control.Monad        (mzero)
+import           Data.Aeson
+import           Data.ByteString.Lazy as LBS
+import qualified Data.Map             as M
+import           Data.Monoid          ((<>))
+import qualified Data.Text            as T
+import           Network.Wreq
 
 -- model domain
 -- create http request
@@ -10,16 +16,28 @@ import Data.ByteString.Lazy as LBS
 
 -- astronomy, atmosphere, item.condition`
 data Weather = Weather {
-    sunrise :: String
-  , sunset  :: String
-}
-
-
--- fromJSON instance here
-
-data WeatherResponse = WeatherResponse {
-    response :: Weather
+    sunrise :: !T.Text
+  , sunset  :: !T.Text
+  , date    :: !T.Text
+  , temp    :: !T.Text
+  , fcast   :: !T.Text
 } deriving (Show)
 
 
-runRequest :: T.Text -> IO ByteString
+instance FromJSON Weather where
+    parseJSON (Object o) =
+        Weather <$> (astronomy >>= (.:"sunrise"))
+                <*> (astronomy >>= (.: "sunset"))
+                <*> (item >>= (.: "date"))
+                <*> (item >>= (.: "temp"))
+                <*> (item >>= (.: "text"))
+        where e = ((o .: "query") >>= (.:"results") >>= (.: "channel"))
+              astronomy = (e >>= (.:"astronomy"))
+              item = (e >>= (.:"item") >>= (.:"condition"))
+
+
+    parseJSON _ = mzero
+
+        -- >>= (.: "results") >>= (.: "channel") >>= (.: "astronomy") >>= (.: "sunrise"))
+
+endPoint = "https://query.yahooapis.com/v1/public/yql?q=select%20astronomy%2C%20%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22dallas%2C%20tx%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
