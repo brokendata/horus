0c1bf86950ee8481745be3a8f48b4c1425845275
Ryan Wheeler <ryanmwheeler@gmail.com>
2015-09-01 20:44:35 -0500

    create function to build a query

--- src/Network/Yahoo/Weather.hs
+++ src/Network/Yahoo/Weather.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Applicative  ((<$>), (<*>))
+module Horus
+  (
+  -- functions here
+  ) where
+
+import           Control.Applicative        ((<$>), (<*>))
 import           Control.Lens
-import           Control.Monad        (mzero)
+import           Control.Monad              (mzero)
 import           Data.Aeson
-import           Data.ByteString.Lazy as LBS
-import qualified Data.Map             as M
-import           Data.Monoid          ((<>))
-import qualified Data.Text            as T
+import           Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Map                   as M
+import           Data.Monoid                ((<>))
+import qualified Data.Text                  as T
 import           Network.Wreq
 
 -- model domain
@@ -31,9 +36,9 @@ instance FromJSON Weather where
                 <*> (item >>= (.: "date"))
                 <*> (item >>= (.: "temp"))
                 <*> (item >>= (.: "text"))
-        where e = ((o .: "query") >>= (.:"results") >>= (.: "channel"))
-              astronomy = (e >>= (.:"astronomy"))
-              item = (e >>= (.:"item") >>= (.:"condition"))
+        where e = (o .: "query") >>= (.:"results") >>= (.: "channel")
+              astronomy = e >>= (.:"astronomy")
+              item = e >>= (.:"item") >>= (.:"condition")
 
 
     parseJSON _ = mzero
@@ -41,3 +46,19 @@ instance FromJSON Weather where
         -- >>= (.: "results") >>= (.: "channel") >>= (.: "astronomy") >>= (.: "sunrise"))
 
 endPoint = "https://query.yahooapis.com/v1/public/yql?q=select%20astronomy%2C%20%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22dallas%2C%20tx%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+
+constructQuery :: String -> String -> String
+constructQuery city state = "select astronomy,  item.condition from weather.forecast" <>
+                            " where woeid in (select woeid from geo.places(1)" <>
+                            " where text=\"" <> city <> "," <> state <> "\")"
+
+runRequest :: String -> IO ByteString
+runRequest yql = do
+    r <- get yql
+    return $ r ^. responseBody
+
+
+-- run :: String -> IO (Maybe Weather)
+run yql = do
+    w <- runRequest yql
+    return $ decode w :: IO (Maybe Weather)
