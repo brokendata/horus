348bdd723788299c6f3b1dadb46b2a2688f33c32
Ryan Wheeler <ryanmwheeler@gmail.com>
2015-09-06 14:15:59 -0500

    added ip lookup

--- src/Network/Yahoo/Weather.hs
+++ src/Network/Yahoo/Weather.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 module Horus
   (
@@ -17,13 +14,11 @@ import qualified Data.Map                   as M
 import           Data.Monoid                ((<>))
 import qualified Data.Text                  as T
 import           Network.Wreq
+import GHC.Generics
+import Control.Arrow ((&&&))
+import Control.Monad
 
--- model domain
--- create http request
--- parse with aeson
--- yql string select item.condition.text from weather.forecast where woeid in (select woeid from geo.places(1) where text="dallas, tx")
 
--- astronomy, atmosphere, item.condition`
 data Weather = Weather {
     sunrise :: !T.Text
   , sunset  :: !T.Text
@@ -32,6 +27,20 @@ data Weather = Weather {
   , fcast   :: !T.Text
 } deriving (Show)
 
+data IpResponse = IpResponse {
+    ip       :: !T.Text
+  , hostname :: !T.Text
+  , city     :: !T.Text
+  , region   :: !T.Text
+  , country  :: !T.Text
+  , loc      :: !T.Text
+  , org      :: !T.Text
+  , postal   :: !T.Text
+  , phone    :: Int
+
+} deriving (Show, Generic)
+
+instance FromJSON IpResponse
 
 instance FromJSON Weather where
     parseJSON (Object o) =
@@ -47,6 +56,12 @@ instance FromJSON Weather where
 
     parseJSON _ = mzero
 
+-- need to convert Get exception to Nothing
+getLocation:: IO (Maybe IpResponse)
+getLocation = do
+    r <- get "http://ipinfo.io/json"
+    let body = r ^. responseBody
+    return (decode body :: Maybe IpResponse)
 
 constructQuery :: T.Text -> T.Text -> T.Text
 constructQuery city state = "select astronomy,  item.condition from weather.forecast" <>
@@ -66,13 +81,20 @@ buildRequest yql = do
 run :: T.Text -> IO (Maybe Weather)
 run yql = buildRequest yql >>= (\r -> return $ decode r :: IO (Maybe Weather))
 
-
 dallas :: T.Text
 dallas = constructQuery "dallas" "tx"
 
 denton :: T.Text
-denton = constructQuery "denton" "tx"
+denton = constructQuery "Denton" "Texas"
 
 -- Quick Examples
 runDenton = run denton
 runDallas = run dallas
+
+runMyLocation:: IO (Maybe Weather)
+runMyLocation = do
+    r <- getLocation
+    case r of
+        Just ip -> build ip
+        _ ->  return Nothing
+    where build = (run . (uncurry constructQuery) . (city &&& region))
